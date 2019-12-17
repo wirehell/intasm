0a3ae0e083833460fc65d4460279b2b52e4247c0
Kenny Johansson <wirehell@gmail.com>
2019-12-17 21:28:02 +0100

    GOL example

--- examples/gol.asm
+++ examples/gol.asm
@@ -3,17 +3,18 @@
 
 #def    world_size x_size * y_size
 
-#def    new_cell 79
-#def    old_cell 80
+#def    cell 79
+#def    space 46
 
-#def    new_space 46
-#def    old_space 47
+#def    OW  999999          ; Overwritten pointers
 
         jit [true], init    ; Entry point
 
 ; ------------------ Global vars -----------------------------
 .false  dw 0                ; Helper for non conditional jumps
 .true   dw 1                ;   - " -
+
+.tmp    dw 0
 
 .source dw 0                ; Pointer to current buffer
 .dest   dw 0                ; Pointer to next gen buffer
@@ -36,16 +37,93 @@
 .copyi  add [0], 0, [0]                     ; Dummy values, but modes needs to be correct
 
         eq world, [i1], [ieq]
-        jit [ieq], l                         ; while i1 != dest (copy from top to bottom)
+        jif [ieq], l                         ; while i1 != dest (copy from top to bottom)
 
 ;----------------------- Init End ------------------------------
         jit [true], print
 
-.main   halt
-; Evaluate source to dest
-; Flip buffers
-; Print
-        jif [false], main
+;-------------------------Evolve --------------------------------
+
+.ei     dw 0
+.esi    dw 0
+.edi    dw 0
+.nc     dw 0
+.nbres  dw 0
+.nbptr  dw 0
+
+.evolve add [source], world_size, [esi]
+        add [dest], world_size, [edi]
+        add world_size, 0, [ei]
+
+.eloop  add [esi], -1, [esi]
+        add [edi], -1, [edi]
+        add [ei], -1, [ei]
+
+; Iterate over neighbours
+        add 0, 0, [nc]          ; neighbour count = 0
+
+;; Above
+        add [esi], - x_size - 1, [nb0+1]
+.nb0    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+        add [esi], -x_size, [nb1+1]
+.nb1    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+        add [esi], -x_size + 1 , [nb2+1]
+.nb2    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+;; Left|Right
+        add [esi], -1 , [nb3+1]
+.nb3    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+        add [esi], 1 , [nb4+1]
+.nb4    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+;; Below
+
+        add [esi], x_size - 1, [nb5+1]
+.nb5    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+        add [esi], x_size, [nb6+1]
+.nb6    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+        add [esi], x_size + 1 , [nb7+1]
+.nb7    eq [OW], cell, [nbres]
+        add [nbres], [nc], [nc]
+
+
+; Life or death calculations
+
+        eq [nc], 3, [tmp]       ; If 3, always live
+        jit [tmp], clive
+
+        eq [nc], 2, [tmp]       ; If 2, copy
+        jit [tmp], ccopy
+                                ;else kill
+        add [edi], 0, [cdw+3] ;
+.cdw    add space, 0, [OW] ; Cell death
+        jit [true], cdone
+
+.clive   add [edi], 0, [cbw+3] ;
+.cbw    add cell, 0, [OW] ; Cell born or kept alive
+        jit [true], cdone
+
+.ccopy  add [esi], 0, [nochw+1] ; Set read address
+        add [edi], 0, [nochw+3] ; Set write address
+.nochw  add [esi], 0, [edi]     ; Copy cell
+
+.cdone  jit [ei], eloop
+        jit [true], print
+
+
+;------------------------Evolve End ----------------------------
 
 
 ;--------------------- Printing ----------------------------
@@ -54,9 +132,8 @@
 .pi     dw 0
 .pc     dw 0                                ; Conditions
 .base   dw 0
-.tmp    dw 0
 
-.print  add world, 0, [pi]                  ; i = dest
+.print  add [dest], 0, [pi]     ; i = dest
         add y_size, 0, [py]                 ; y = y_size
 .ply    add [py], -1, [py]                  ; y = y-1
         add x_size, 0, [px]                 ; x = x_size
@@ -78,9 +155,18 @@
         jit [py], ply                       ; while y != 0
 
         ; output ball                       ; for sync and non breaking
+        out 0
+        out 0
+        out 3
         ; output paddle                     :  - " -
+        out 0
+        out 0
+        out 4
 
-        jit [true], main                   ;return
+        add [source], 0, [tmp]
+        add [dest], 0, [source]
+        add [tmp], 0, [dest]
+        jit [true], evolve                   ;return
 
 ; -------------------- End of Printing ---------------------------
 
